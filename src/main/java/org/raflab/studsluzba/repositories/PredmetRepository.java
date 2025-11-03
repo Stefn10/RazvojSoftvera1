c1243a24a5b8dec0b09f23cfb7516c82aa7f2c8d
Stefke <stefnnestor10@gmail.com>
2025-11-03 23:25:33 +0100

    Drzim Palceve da sve radi

--- src/main/java/org/raflab/studsluzba/repositories/PredmetRepository.java
+++ src/main/java/org/raflab/studsluzba/repositories/PredmetRepository.java
@@ -1,14 +1,18 @@
 package org.raflab.studsluzba.repositories;
 
-import java.util.List;
-
 import org.raflab.studsluzba.model.Predmet;
 import org.raflab.studsluzba.model.StudijskiProgram;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PredmetRepository extends CrudRepository<Predmet, Long> {
+	
+	@Query("select p from Predmet p where p.studProgram.godinaAkreditacije = :godinaAkreditacije")
+	List<Predmet> getPredmetForGodinaAkreditacije(Integer godinaAkreditacije);
 
 	List<Predmet> getPredmetsByStudProgramAndObavezan(StudijskiProgram studProgram, boolean obavezan);
 
